d9969011962c33464968fa2bf4c78cbfec2a172d
MePengusta <umt955@gmail.com>
2020-12-12 19:56:18 +0300

    log

--- src/main/java/umut/backend/BackendApplication.java
+++ src/main/java/umut/backend/BackendApplication.java
@@ -9,5 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
+        System.out.println(System.getenv("MAILJET_API_SECRET_KEY"));
+        System.out.println(System.getenv("MAILJET_API_KEY"));
     }
 }
