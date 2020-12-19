17b1d718fb07ba9d35ee31641f7bfb746649c1a2
MePengusta <umt955@gmail.com>
2020-12-19 16:55:41 +0300

    test

--- src/main/java/umut/backend/Controllers/HerokuController.java
+++ src/main/java/umut/backend/Controllers/HerokuController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/heroku")
 @CrossOrigin
@@ -14,6 +16,7 @@ public class HerokuController {
 
     @GetMapping
     public void heroku() {
+        log.info(new Date().toString());
         log.info("Heroku wake up.");
     }
 }
